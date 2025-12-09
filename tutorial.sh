9cf36a91e8ec31bb535fa8ff6d313d3e5d494577
WolfofAstora <beni.masoldt@gmx.de>
2025-12-09 16:12:48 +0100

    Setting foundation

--- tutorial.sh
+++ tutorial.sh
@@ -1,27 +1,63 @@
 #!/bin/bash
-
 checkCreateJSON() {
 	if ! [ -f ./todos.json ]; then
 		echo -e "File does not exists\n Creating todos.json..."
-		touch todos.json
+		echo -e "[\n\n]" >> todos.json
+
 	fi
 }
 
+getJSONContent(){
+	local data=(cat todos.json)
+	echo $data
+}
 
-if [ $# -eq 0 ]; then
-	echo "no parameters where given"
-fi
+addToJSON(){	
 
+	structure="{
+			\"id\" : 1,\n
+			\"description\" : $2,\n
+			\"status\" : \"todo\",\n
+			\"createdAt\" : \"date\",\n
+			\"updatedAt\" : \"date\"\n
+		   }"
+	echo $structure >> todos.json
 
+}
 
-if [ "$1" = 'add' ]; then
-	checkCreateJSON
-	echo "add has been typed wuhu"
-	if [ $# -eq 2 ]; then
-		echo "$2 will be added to your todo list heheha"
-		
-	else
-		echo "add needs a task to be added to your todo list"
+addTask(){	
+	if [[ "$1" = 'add' && $# -eq 2 ]]; then
+		echo "$2 will be added to your todo list"
+		getJSONContent #debug
+		# addToJSON $2
 	fi
-fi
+}
 
+noParameter(){
+	#TODO: not working, maybe replace with help if no arguments where given
+	if [ $# -eq 0 ]; then
+		echo "no parameters where given"
+		exit 
+	fi
+}
+main(){	
+	noParameter $#
+	checkCreateJSON
+	addTask $1 $2
+}
+
+main "$@"
+
+
+
+
+# [
+#   {
+#      "id" : 1,
+#      "description" : "djalsdfjas",
+#      "status" : "todo",
+#      "createdAt" : "12.12.12",
+#      "updatedAt" : "13.12.12"
+#   },
+#   ...
+# ]
